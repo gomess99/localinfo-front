be3c2d725e36b9dc3e44909cefbda81345c3ea82
UarliBS <warllen2014@gmail.com>
2023-11-16 00:37:52 -0300

    Ajustando input de pesquisa

--- src/components/BusqueLocal/BusqueLocal.jsx
+++ src/components/BusqueLocal/BusqueLocal.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
 import "../BusqueLocal/BusqueLocal.css";
 
 function BusqueLocal() {
+  const { register, handleSubmit, reset } = useForm();
+
+  function onSearch(data) {
+    const {title} = data;
+    console.log(data);
+  }
 
   return (
     <div className="busquelocal-frame">
       <h1>Busque por sua localização</h1>
       <div className="busquelocal-pesquisas">
-        <div className="search-box">
-          <input type="text" className="search-text" placeholder="Digite sua busca..." />
-          <a className="search-btn">
-            <FontAwesomeIcon icon={faSearch} />
-          </a>
-        </div>
+        <form onSubmit={handleSubmit(onSearch)}>
+          <div className="search-box">
+            <input {...register("title")}type="text" className="search-text" placeholder="Digite sua busca..." />
+            <button type="submit" className="search-btn">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+        </form>
 
         <div className="busquelocal-local">
           <div className="busquelocal-lupa"></div>
