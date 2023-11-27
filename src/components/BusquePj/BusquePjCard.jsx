8ac5430daad0624b0e052f9b339e7f7e233ab9a0
gomess99 <gomess99@outlook.com>
2023-11-27 14:36:00 -0300

    Merge branch 'main' of https://github.com/gomess99/localinfo-front

--- src/components/BusquePj/BusquePjCard.jsx
+++ src/components/BusquePj/BusquePjCard.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import "../BusquePj/BusquePj.css";
 
-export function BusquePjCard({
-  name,
-  categoria
-}) {
-  
+export function BusquePjCard({ name, categoria, likes }) {
   return (
     <div className="busquepj-card">
       <div className="busquepj-card-img"></div>
 
       <div className="busquepj-card-info">
         <div className="busquepj-perfil"></div>
-
-        <p>{name}</p>
-        <p>{categoria}</p>
-
-        <div className="busquepj-icon"></div>
+        <div className="busquepj-text">
+          <div className="busquepj-name">
+            <h1>
+              {name} 
+            </h1>
+            <div className="busquepj-icon"></div>
+          </div>
+          <p>Likes: {likes}</p>
+          <p>{categoria}</p>
+        </div>
+        <div className="busquepj-like"></div>
       </div>
     </div>
   );
