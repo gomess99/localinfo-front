ce6a89095994c651e2a7752304fe3fad99abb823
gomess99 <gomess99@outlook.com>
2023-12-11 19:43:20 -0300

    Retorno completo do usuário logado

--- src/schemas/signinSchema.js
+++ src/schemas/signinSchema.js
@@ -4,5 +4,5 @@ export const signinSchema = z.object({
   email: z.string().email({ message: "E-mail inválido" }).toLowerCase(),
   password: z
     .string()
-    .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
+    .min(6, { message: "E-mail ou senha inválidos" }),
 });
