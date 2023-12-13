ed9c6f256f63720539c7584b20c48900563759e6
gomess99 <gomess99@outlook.com>
2023-12-12 21:25:36 -0300

    Alteração nas totas de App e router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Pages/Home/Home";
-import AboutUs from "./components/Pages/AboutUs/AboutUs";
-import FindBusiness from "./components/Pages/FindBusiness/FindBusiness";
-import Plan from "./components/Pages/Plan/Plan";
-import RegisterPj from "./components/Pages/UserRegister/RegisterPf_Pj/RegisterPj";
-import RegisterPf from "./components/Pages/UserRegister/RegisterPf_Pj/RegisterPf";
-import PerfilPf from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPf";
-import PerfilPj from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPj";
-import PerfilPj_PlanoFree from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPj_PlanoFree";
-import SucessRegisterPf_Pj from "./components/Pages/UserRegister/SucessRegister/SucessRegisterPf_Pj";
-import SucessRegisterPj from "./components/Pages/UserRegister/SucessRegister/SucessRegisterPj";
-import Free from "./components/Pages/Plan/Free/Free";
-import Premium from "./components/Pages/Plan/Premium/Premium";
-import Categories from "./components/Pages/Categories/Categories";
-import Login from "./components/Pages/Login/Login";
-import NewPassword from "./components/Pages/Login/NewPassword";
-import UserRegister from "./components/Pages/UserRegister/UserRegister";
-import SearchCategories from "./components/Pages/SearchCategories/searchCategories";
+import Home from "./components/Pages/Home/Home.jsx";
+import AboutUs from "./components/Pages/AboutUs/AboutUs.jsx";
+import FindBusiness from "./components/Pages/FindBusiness/FindBusiness.jsx";
+import Plan from "./components/Pages/Plan/Plan.jsx";
+import RegisterPj from "./components/Pages/UserRegister/RegisterPf_Pj/RegisterPj.jsx";
+import RegisterPf from "./components/Pages/UserRegister/RegisterPf_Pj/RegisterPf.jsx";
+import PerfilPf from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPf.jsx";
+import PerfilPj from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPj.jsx";
+import PerfilPj_PlanoFree from "./components/Pages/UserRegister/PerfilPf_Pj/PerfilPj_PlanoFree.jsx";
+import SucessRegisterPf_Pj from "./components/Pages/UserRegister/SucessRegister/SucessRegisterPf_Pj.jsx";
+import SucessRegisterPj from "./components/Pages/UserRegister/SucessRegister/SucessRegisterPj.jsx";
+import Free from "./components/Pages/Plan/Free/Free.jsx";
+import Premium from "./components/Pages/Plan/Premium/Premium.jsx";
+import Categories from "./components/Pages/Categories/Categories.jsx";
+import Login from "./components/Pages/Login/Login.jsx";
+import NewPassword from "./components/Pages/Login/NewPassword.jsx";
+import UserRegister from "./components/Pages/UserRegister/UserRegister.jsx";
+import SearchCategories from "./components/Pages/SearchCategories/searchCategories.jsx";
 
 const App = () => {
   return (
